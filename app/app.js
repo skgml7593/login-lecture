d9ca48fb60167c3918de8cf9deeb1241a83d8e01
LeeNaHee <skgml7593@gmail.com>
2022-03-28 19:38:52 +0900

    프런트 로그인 기능

--- app/app.js
+++ app/app.js
@@ -1,21 +1,8 @@
-// const http = require('http');
-// const app = http.createServer((req, res) => {
-//     res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"})
-//     if(req.url === '/'){
-//         res.end("여기는 루트입니다.");
-//     } else if(req.url === '/login'){
-//         res.end("여기는 로그인 화면입니다.");
-//     }
-// });
-
-// app.listen(3004, ()=>{
-//     console.log("http로 가동된 서버입니다.");
-// });
 
 // 모듈
 const express = require('express');
+//const bodyParser = require("body-parser");
 const app = express();
-
 
 // 라우팅
 const home = require("./src/routes/home");
@@ -23,6 +10,11 @@ const home = require("./src/routes/home");
 // 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+
+app.use(express.static(`${__dirname}/src/public`));
+app.use(express.json());
+// URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
+app.use(express.urlencoded({extended : true}));
 
 app.use("/", home);  
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,13 +1,44 @@
 "use strict"
-const hello = (req, res)=>{
-    res.render("home/index");
-}
 
-const login = (req, res)=>{
-    res.render("home/login");
-}
+const users = {
+    id: ["1234", "12345", "123"],
+    psword: ["1234", "12345","123"], 
+};
+
+const output = {
+    home: (req, res)=>{
+        res.render("home/index");
+    },
+    
+    login: (req, res)=>{
+        res.render("home/login");
+    },
+
+};
+
+const process = {
+    login: (req, res) =>{
+        const id = req.body.id,
+        psword = req.body.psword;
+
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] === psword){
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패했습니다.",
+        });
+    },
+};
+
 
 module.exports = {
-    hello,
-    login
+    output,
+    process,
 };
